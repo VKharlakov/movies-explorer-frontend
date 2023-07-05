0846e56d924d568136d74a4b6c967702cfd3a126
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-07-05 20:38:11 +0200

    Диплом-фронт: вернуть адрес api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const USER_NAME_REGEX = '^[a-zA-Zа-яА-ЯёЁ\\s\\-]+$';
 
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://bitfilms.kharlakov.api.nomoredomains.rocks';
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const SHORTMOVIES_DURATION = 40;
 export const DEVICE_PARAMS = {
